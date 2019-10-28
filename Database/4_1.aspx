e891ce25465713ddb16b24ca742fa8ce5232a77b
1938917626@qq.com <1938917626@qq.com>
2019-10-28 10:55:41 +0800

    4_1 查询数据库连接信息connection

---
+++ Database/4_1.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="4_1.aspx.cs" Inherits="Database._4_1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Text="显示信息："></asp:Label>
+            <br />
+            <asp:Button ID="btnSM" runat="server" OnClick="btnSM_Click" Text="连接数据库" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Database/4_1.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace Database
+{
+    public partial class _4_1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSM_Click(object sender, EventArgs e)
+        {
+            string str = ConfigurationManager.ConnectionStrings["SMDB"].ConnectionString;
+            using(SqlConnection conn=new SqlConnection(str))
+            {
+                try
+                {
+                    conn.Open();
+                    Label1.Text = "连接字符串:" + conn.ConnectionString;
+                    Label1.Text += "<br/>连接状态:" + conn.State.ToString();
+                    Label1.Text += "<br/>数据源:" + conn.DataSource;
+                    Label1.Text += "<br/>服务器版本:" + conn.ServerVersion;
+                    Label1.Text += "<br/>数据库名称:" + conn.Database;
+                }
+                catch(Exception ee)
+                {
+                    Label1.Text = "连接失败原因为：" + ee.Message;
+                }
+                finally
+                {
+                    conn.Dispose();
+                }
+            }
+        }
+    }
+}
