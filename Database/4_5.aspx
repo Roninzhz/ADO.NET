d9c4ca6a2309f23f6448c45d171db567729846f6
1938917626@qq.com <1938917626@qq.com>
2019-11-01 15:58:14 +0800

    添加4_5网页 查询信息

---
+++ Database/4_5.aspx
@@ -0,0 +1,47 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="4_5.aspx.cs" Inherits="Database._4_5" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 100%;
+        }
+        .auto-style2 {
+            width: 252px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <table class="auto-style1">
+            <tr>
+                <td class="auto-style2">根据指定列名读取指定用户的信息</td>
+                <td rowspan="5">
+                    <asp:Label ID="lblInfo" runat="server" Text="显示指定用户信息："></asp:Label>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style2">列名：<asp:DropDownList ID="ddlCols" runat="server" AutoPostBack="True" OnSelectedIndexChanged="ddlCols_SelectedIndexChanged">
+                    </asp:DropDownList>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style2">数据项：<asp:ListBox ID="ltbItems" runat="server" AutoPostBack="True" OnSelectedIndexChanged="ltbItems_SelectedIndexChanged"></asp:ListBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style2">&nbsp;</td>
+            </tr>
+            <tr>
+                <td class="auto-style2">&nbsp;</td>
+            </tr>
+        </table>
+        <div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Database/4_5.aspx.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Database
+{
+    public partial class _4_5 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                ddlColsData();
+                ItbItemsData();
+            }
+        }
+
+        ///<summary>
+        ///设定下拉列表ddlCols的数据内容
+        /// </summary>
+        public void ddlColsData()
+        {
+            ddlCols.Items.Clear();
+            string str = ConfigurationManager.ConnectionStrings["SMDB"].ConnectionString;
+            using(SqlConnection conn=new SqlConnection(str))
+            {
+                conn.Open();
+                string cmdText = "select uName,uRealName,uPhone,uQQ from users";
+                SqlCommand cmd = new SqlCommand(cmdText, conn);
+                SqlDataReader dr = cmd.ExecuteReader();
+                DataTable dt = dr.GetSchemaTable();
+                ddlCols.Items.Add(new ListItem("用户名", dt.Rows[0][0].ToString()));
+                ddlCols.Items.Add(new ListItem("姓名", dt.Rows[1][0].ToString()));
+                ddlCols.Items.Add(new ListItem("联系电话", dt.Rows[2][0].ToString()));
+                ddlCols.Items.Add(new ListItem("QQ号", dt.Rows[3][0].ToString()));
+            }
+        }
+
+        ///<summary>
+        ///设定列表框ltItems的数据内容
+        /// </summary>
+        /// 
+        public void ItbItemsData()
+        {
+            ltbItems.Items.Clear();
+            string str = ConfigurationManager.ConnectionStrings["SMDB"].ConnectionString;
+            using(SqlConnection conn=new SqlConnection(str))
+            {
+                conn.Open();
+                string filed = ddlCols.SelectedValue;
+                string cmdText = "select uID," + filed + " from users";
+                SqlCommand cmd = new SqlCommand(cmdText, conn);
+                SqlDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    ltbItems.Items.Add(new ListItem(dr.GetValue(1).ToString(), dr.GetValue(0).ToString()));
+                }
+            }
+        }
+
+        protected void ddlCols_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ItbItemsData();
+        }
+
+        protected void ltbItems_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            lblInfo.Text = "";
+            string str = ConfigurationManager.ConnectionStrings["SMDB"].ConnectionString;
+            using (SqlConnection conn = new SqlConnection(str))
+            {
+                conn.Open();
+                String cmdText = "select * from users where uID=@uid";
+                SqlCommand cmd = new SqlCommand(cmdText, conn);
+                SqlParameter sp = new SqlParameter("uID", ltbItems.SelectedValue);
+                cmd.Parameters.Add(sp);
+                SqlDataReader dr = cmd.ExecuteReader();
+                if (dr.Read())
+                {
+                    StringBuilder sblstr = new StringBuilder("");
+                    sblstr.Append("<br/>用户名：" + dr["uName"]);
+                    sblstr.Append("<br/>姓名：" + dr["uRealName"]);
+                    sblstr.Append("<br/>性别：" + dr["uSex"]);
+                    sblstr.Append("<br/>年龄：" + dr["uAge"]);
+                    sblstr.Append("<br/>联系电话：" + dr["uPhone"]);
+                    sblstr.Append("<br/>电子邮箱：" + dr["uEmail"]);
+                    lblInfo.Text = sblstr.ToString();
+                }
+            }
+        }
+    }
+}
