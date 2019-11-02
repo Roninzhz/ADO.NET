dea4383c465bd6b232c9075e8193af5da47f98f4
1938917626@qq.com <1938917626@qq.com>
2019-11-02 19:56:04 +0800

    添加4-6web窗体

---
+++ Database/4_6.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="4_6.aspx.cs" Inherits="Database._4_6" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="lblView" runat="server" Text="查看商品信息表中的数据"></asp:Label>
+            <br />
+            <asp:Button ID="btnView" runat="server" OnClick="btnView_Click" Text="查看" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Database/4_6.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Text;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Database
+{
+    public partial class _4_6 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnView_Click(object sender, EventArgs e)
+        {
+            DataSet ds = new DataSet();
+            string str = ConfigurationManager.ConnectionStrings["SMDB"].ConnectionString;
+            using(SqlConnection conn=new SqlConnection(str))
+            {
+                conn.Open();
+                string cmdText = "select top 5 gdCode,gdName,gdPrice,gdQuantity,gdSaleQty from goods";
+                SqlDataAdapter sda = new SqlDataAdapter(cmdText, conn);
+                sda.Fill(ds, "goods");
+            }
+            DataTable dt = ds.Tables["goods"];
+            StringBuilder stblder = new StringBuilder("");
+            foreach(DataColumn col in dt.Columns)
+            {
+                stblder.Append(string.Format("[{0}]", col.ColumnName));
+            }
+            foreach(DataRow row in dt.Rows)
+            {
+                stblder.Append("<br/>");
+                foreach(DataColumn col in dt.Columns)
+                {
+                    stblder.Append(string.Format("-{0}-", row[col]));
+                }
+                lblView.Text = stblder.ToString();
+            }
+        }
+    }
+}
