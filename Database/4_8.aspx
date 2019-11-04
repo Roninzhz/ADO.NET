6ac051ebc51fab8fe8c9e88476e2fceb2d6ba97b
1938917626@qq.com <1938917626@qq.com>
2019-11-04 10:45:34 +0800

    添加绑定数据集合窗体4_8

---
+++ Database/4_8.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="4_8.aspx.cs" Inherits="Database._4_8" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            绑定集合数据<br />
+            <asp:DropDownList ID="ddlGoodType" runat="server">
+            </asp:DropDownList>
+            <asp:CheckBoxList ID="chkGoodType" runat="server">
+            </asp:CheckBoxList>
+            <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Database/4_8.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+namespace Database
+{
+    public partial class _4_8 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SetBind();
+        }
+        protected void SetBind()
+        {
+            string str = ConfigurationManager.ConnectionStrings["SMDB"].ConnectionString;
+            DataSet ds = new DataSet();
+            using(SqlConnection conn=new SqlConnection(str))
+            {
+                conn.Open();
+                string cmdText = "select tName,tID from goodsType";
+                SqlDataAdapter sda = new SqlDataAdapter(cmdText, conn);
+                sda.Fill(ds, "gtype");
+            }
+            ddlGoodType.DataSource = ds.Tables["gtype"];
+            ddlGoodType.DataTextField = "tName";
+            ddlGoodType.DataValueField = "tID";
+            ddlGoodType.DataBind();
+            chkGoodType.DataSource = ds.Tables["gtype"];
+            chkGoodType.DataTextField = "tName";
+            chkGoodType.DataValueField = "tID";
+            chkGoodType.DataBind();
+        }
+    }
+}
