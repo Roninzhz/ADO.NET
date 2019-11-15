ae2adfba418a889f0209580121fca728ee277b39
1938917626@qq.com <1938917626@qq.com>
2019-11-15 15:38:00 +0800

    添加4_11存储过程添加产品类型

---
+++ Database/4_11.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="4_11.aspx.cs" Inherits="Database._4_11" %>
+
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        &nbsp;
+<p>
+    使用存储过程，添加商品类别：</p>
+<p>
+    -------------------------------------------------</p>
+<p>
+    类别名称：<asp:TextBox ID="txtName" runat="server"></asp:TextBox>
+    <asp:Button ID="btnAddType" runat="server" OnClick="btnAddType_Click" Text="添加" />
+</p>
+
+        <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+        <div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Database/4_11.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Database
+{
+    public partial class _4_11 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnAddType_Click(object sender, EventArgs e)
+        {
+            string str = ConfigurationManager.ConnectionStrings["SMDB"].ConnectionString;
+            using (SqlConnection conn = new SqlConnection(str))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("upAddGoodsType", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                SqlParameter[] ps = {new SqlParameter("@tname",txtName.Text),
+                new SqlParameter("@flag",SqlDbType.Int)};
+                ps[1].Direction = ParameterDirection.ReturnValue;
+                cmd.Parameters.AddRange(ps);
+                cmd.ExecuteNonQuery();
+                int tId = (int)cmd.Parameters["@flag"].Value;
+                Label1.Text = txtName.Text + "的类别Id为：" + tId;
+            }
+        }
+    }
+}
